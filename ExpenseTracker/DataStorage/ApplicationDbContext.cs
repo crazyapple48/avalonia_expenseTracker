324fe06a25f02577c8e9f0c7e409849af58b7254
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2026-01-14 18:36:55 -0700

    add entity framework and dbcontext

---
+++ ExpenseTracker/DataStorage/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using ExpenseTracker.DataStorage.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.DataStorage;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<ShortcutDataModel> Shortcuts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Ensure folder exists
+        var storagePath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Expense Tracker");
+        Directory.CreateDirectory(storagePath);
+
+        optionsBuilder.UseSqlite(
+            $"Data Source={Path.Combine(storagePath, "settings.db")}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ShortcutDataModel>().HasKey(f => f.Name);
+    }
+}
