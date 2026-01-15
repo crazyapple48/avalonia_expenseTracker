cfd326191f6ddb08008329ef98b96dd8e66a46a5
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2026-01-14 18:49:29 -0700

    finish database setup

---
+++ ExpenseTracker/DataStorage/DatabaseService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExpenseTracker.DataStorage.DataModels;
+
+namespace ExpenseTracker.DataStorage;
+
+public class DatabaseService(ApplicationDbContext context) : IDisposable
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public void Dispose() => _context.Dispose();
+
+    public void ApplyMigrations()
+    {
+        // TODO: Change to migrations once we start persisting data
+        _context.Database.EnsureCreated();
+    }
+
+    public List<ShortcutDataModel>? GetShortcuts()
+    {
+        return _context.Shortcuts.ToList();
+    }
+
+    public void CreateShortcut(ShortcutDataModel? shortcut)
+    {
+        if (shortcut is null) return;
+        _context.Shortcuts.Add(shortcut);
+    }
+}
